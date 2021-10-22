4f3d3c26b9d14229ea2109635eb15c6b38883c14
chvineeth <chinigapalli.vineeth@gmail.com>
2021-10-22 12:27:37 +0530

    today

---
+++ OrangeHRM12/src/test/java/vineeth/NewTest.java
@@ -0,0 +1,9 @@
+package vineeth;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+  }
+}
---
+++ OrangeHRM12/src/test/java/vvvv/NewTest.java
@@ -0,0 +1,9 @@
+package vvvv;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+  }
+}
